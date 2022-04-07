2486b32d9a7b9962bfeec0cca887f2183d0df4b6
lateef <olulat01@gmail.com>
2022-04-07 21:25:45 +0100

    all put together

--- js/app.js
+++ js/app.js
@@ -1,13 +1,44 @@
-const toggler = document.querySelectorAll("#toggle-btn");
+const toggler = document.querySelector("#toggle-btn");
+const closeBtn = document.querySelector("#toggle-close-btn");
 const sideNav = document.querySelector(".side-nav")
 const toggleIcon = document.querySelector(".toggle-icon")
+const bottomNav = document.querySelector(".bottom-nav")
+const clickAnyToClose = document.querySelectorAll(".to-close")
 
-toggler.forEach((item) =>
-item.addEventListener("click", function (){
+
+toggler.addEventListener("click", function(){
     sideNav.classList.toggle("side-nav-active")
     toggleIcon.classList.toggle("icon-primary")
+    bottomNav.classList.toggle("bottom-nav-fixed")
+    document.body.classList.toggle("scroll-stop")
+    
+    // if(sideNav.classList.contains("side-nav-active")){
+    //     bottomNav.classList.add("bottom-nav-fixed")
+    //     document.body.classList.add("scroll-stop")
+    // }else
+    // {
+    //    bottomNav.classList.remove("bottom-nav-fixed")
+    //    document.body.classList.remove("scroll-stop") 
+    // }
+    
+})
+
+closeBtn.addEventListener("click", function(){
+    sideNav.classList.remove("side-nav-active")
+    toggleIcon.classList.remove("icon-primary")
+    bottomNav.classList.remove("bottom-nav-fixed")
+    document.body.classList.remove("scroll-stop")
+})
+
+clickAnyToClose.forEach((item) =>
+item.addEventListener("click", function (){
+    sideNav.classList.remove("side-nav-active")
+    bottomNav.classList.remove("bottom-nav-fixed")
+    toggleIcon.classList.remove("icon-primary")
+    document.body.classList.remove("scroll-stop")
 })
 )
+
 
   let slideIndex = 0;
         showSlides();
@@ -55,3 +86,4 @@ item.addEventListener("click", function (){
             slide[slideManual - 1].style.display = "flex";
             dots[slideManual - 1].className += " active";
         }
+
